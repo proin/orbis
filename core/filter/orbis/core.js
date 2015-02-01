32384c36515d6c27d151cbc32fe4bc6f56d8b766
Chae Yeonghun <proin0312@gmail.com>
2015-02-02 00:48:20 +0900

    0.1.8
    adding orbis-syntax, orbis-attr

--- core/filter/orbis/core.js
+++ core/filter/orbis/core.js
@@ -1,58 +1,97 @@
 exports.filter = function (server, session, callback) {
-    var fs = require('fs');
-    data = fs.readFileSync(server.vhost.DIR + server.path) + '';
+	var orbisScriptVariable = {};
 
-    var orbisApi = function (server, session, data, callback) {
-        data += '';
+	var orbisAttr = function (data) {
+		data = data.replace(/\n/g, '');
+		data = data.replace(/<!--.*?-->/gim, '');
 
-        data = data.replace(/\n/g, '');
-        data = data.replace(/<!--.*?-->/gim, '');
+		var re = /orbis-attr="(.*?)"/gim;
+		var finded = re.exec(data);
+		if (finded == null) {
+			callback(200, data);
+			return;
+		}
 
-        var fs = require('fs');
+		data = data.replace(finded[0], orbisScriptVariable.attr[finded[1]] == null ? '' : orbisScriptVariable.attr[finded[1]]);
+		orbisAttr(data);
+	}
 
-        var re = /<orbis.*?role="(.*?)".*?<\/?orbis>/gim;
-        var finded = re.exec(data);
-        if (finded == null) {
-            callback(200, data);
-            return;
-        }
+	var orbisScript = function (data, script) {
+		data = data.replace(/\n/g, '');
+		data = data.replace(/<!--.*?-->/gim, '');
 
-        var role = finded[1];
+		var re = /<script role="orbis">(.*?)<\/?script>/gim;
+		var finded = re.exec(data);
+		if (finded == null) {
+			try {
+				var mscript = 'var orbis = {};' + 'orbis.attr = {};' + script + '\nexports.orbis = orbis;';
+				var Module = module.constructor;
+				var m = new Module();
+				m._compile(mscript);
 
-        if (role == 'api') {
-            global.module.filter.orbis.api.parse(server, session, finded[0], function (result) {
-                re = /<orbis.*?name="(.*?)".*?<\/?orbis>/gim;
-                var valueName = re.exec(finded[0]);
-                valueName = valueName == null ? 'apiResult' : valueName[1];
-                data = data.replace(finded[0], '<script type="text/javascript">var ' + valueName + '=' + JSON.stringify(result) + '</script>');
-                orbisApi(server, session, data, callback);
-            });
-        } else if (role == 'auth') {
-            global.module.filter.orbis.auth.parse(server, session, finded[0], function (auth, result) {
-                if (auth == false) data = data.replace(/<body.*?<\/?body>/gim, '<body>' + result + '</body>');
-                else data = data.replace(finded[0], result);
-                orbisApi(server, session, data, callback);
-            });
-        } else if (role == 'template') {
-            global.module.filter.orbis.template.parse(server, session, finded[0], function (result) {
-                data = data.replace(finded[0], result);
-                orbisApi(server, session, data, callback);
-            });
-        } else if (role == 'query') {
-            global.module.filter.orbis.query.parse(server, session, finded[0], function (result) {
-                data = data.replace(finded[0], result);
-                orbisApi(server, session, data, callback);
-            });
-        } else if (role == 'session') {
-            global.module.filter.orbis.session.parse(server, session, finded[0], function (result) {
-                data = data.replace(finded[0], result);
-                orbisApi(server, session, data, callback);
-            });
-        } else {
-            data = data.replace(finded[0], '');
-            orbisApi(server, session, data, callback);
-        }
-    };
+				Object.keys(m.exports.orbis).forEach(function (key) {
+					orbisScriptVariable[key] = m.exports.orbis[key];
+				});
+			} catch (e) {
+			}
+			orbisAttr(data);
+			return;
+		}
 
-    orbisApi(server, session, data, callback);
+		script += '\n' + finded[1];
+		data = data.replace(finded[0], '');
+		orbisScript(data, script);
+	}
+
+	var orbisTag = function (data) {
+		data = data.replace(/\n/g, '');
+		data = data.replace(/<!--.*?-->/gim, '');
+
+		var re = /<orbis.*?role="(.*?)".*?<\/?orbis>/gim;
+		var finded = re.exec(data);
+		if (finded == null) {
+			orbisScript(data, '');
+			return;
+		}
+
+		var role = finded[1];
+
+		if (role == 'api') {
+			global.module.filter.orbis.api.parse(server, session, finded[0], function (result) {
+				re = /<orbis.*?name="(.*?)".*?<\/?orbis>/gim;
+				var valueName = re.exec(finded[0]);
+				valueName = valueName == null ? 'apiResult' : valueName[1];
+				data = data.replace(finded[0], '<script type="text/javascript">var ' + valueName + '=' + JSON.stringify(result) + '</script>');
+				orbisTag(data);
+			});
+		} else if (role == 'auth') {
+			global.module.filter.orbis.auth.parse(server, session, finded[0], function (auth, result) {
+				if (auth == false) data = data.replace(/<body.*?<\/?body>/gim, '<body>' + result + '</body>');
+				else data = data.replace(finded[0], result);
+				orbisTag(data);
+			});
+		} else if (role == 'template') {
+			global.module.filter.orbis.template.parse(server, session, finded[0], function (result) {
+				data = data.replace(finded[0], result);
+				orbisTag(data);
+			});
+		} else if (role == 'query') {
+			global.module.filter.orbis.query.parse(server, session, finded[0], function (result) {
+				data = data.replace(finded[0], result);
+				orbisTag(data);
+			});
+		} else if (role == 'session') {
+			global.module.filter.orbis.session.parse(server, session, finded[0], function (result) {
+				data = data.replace(finded[0], result);
+				orbisTag(data);
+			});
+		} else {
+			data = data.replace(finded[0], '');
+			orbisTag(data);
+		}
+	};
+
+	var fs = require('fs');
+	data = fs.readFileSync(server.vhost.DIR + server.path) + '';
+	orbisTag(data);
 }
