000692f8dc48b56e13711ace4b16e417c852c1f1
Chae Yeonghun <proin0312@gmail.com>
2015-04-24 14:37:11 +0900

    0.1.11
    update read stream

--- core/filter/stream/index.js
+++ core/filter/stream/index.js
@@ -1,19 +1,19 @@
 exports.filter = function (server, session) {
     var type = require('mime').lookup(server.vhost.dir + server.path);
-    data = require('fs').readFile(server.vhost.dir + server.path, function (err, data) {
-        if (err) {
-            exports.response(server, 500, err, type);
-            return;
-        }
-        exports.response(server, 200, data, type);
-    });
-}
+    server.response.writeHead(200, {'Content-Type': type});
 
-exports.response = function (server, code, result, type) {
-    if (code == 200) {
-        server.response.writeHead(code, {'Content-Type': type});
-        server.response.end(result);
-    } else {
-        server.printError(code, result);
-    }
+    var readStream = require('fs').createReadStream(server.vhost.dir + server.path);
+    readStream.on('data', function (chunk) {
+        if (!server.response.write(chunk)) {
+            readStream.pause();
+        }
+    });
+
+    readStream.on('end', function () {
+        server.response.end();
+    });
+
+    server.response.on('drain', function () {
+        readStream.resume();
+    });
 }
